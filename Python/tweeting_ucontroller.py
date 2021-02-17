c56a0e462e43650016f610dd424d9ea50aae9819
Gregório <30591064+gregorio1212@users.noreply.github.com>
2021-02-17 09:15:28 +0100

    Comments and tweet_limit changed to tweets_posted

--- Python/tweeting_ucontroller.py
+++ Python/tweeting_ucontroller.py
@@ -5,14 +5,17 @@
 import serial
 import tweepy
 
+#Here you put the key, secret, token and, token secret from your Twitter Developer account
 key = "x90redHO7n2gRHn1IpSc8Vcor"
 secret = "CmxFBjpo6uuqFhCGi6NRFAo2U7fdZx3dUmLkRx5Z8lnEa27Dwb"
 access_token = "1352355518086582272-cf3da0erLLD3RvVzUjCM6lCcjmPMcD"
 access_token_secret = "1k1kPXPmilplIHXPvKUis9cZso17j8IA63WhWk4kuDUlQ"
 
+#In this line we use serial to open the serial connection between the board and the laptop
 ser = serial.Serial('COM5', 9600,timeout=1)
-tweet_limit = 0
+tweets_posted = 0
 
+#This is the function used to login in to your Twitter account
 def OAuth():
     try:
         auth = tweepy.OAuthHandler(key, secret)
@@ -24,8 +27,9 @@ def OAuth():
 oauth = OAuth()
 api = tweepy.API(oauth)
 
-while(tweet_limit < 5):
+#Here we keep track of tweets posted so far, once it reaches the limit 5, we stop stop the program
+while(tweet_posted < 5):
     tweet = ser.readline()
     if(tweet !=b''):
         api.update_status(tweet)
-        tweet_limit +=1
+        tweets_posted +=1
